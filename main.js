5c0353470d1493ad36ac92bff101af94e493ae11
mswgen <mswgen02@gmail.com>
2020-04-16 21:29:35 +0900

    서버목록 코드 추가

--- main.js
+++ main.js
@@ -14,6 +14,13 @@ function trueOrFalse (input, yes, no) {
         return no;
     }
 }
+function guilds (guild) {
+    var toReturn = '';
+    for (var x of guild) {
+        toReturn += `${x.name}(서버 id: ${x.id}, 서버 내 권한 코드: ${x.permissions})<br>`;
+    }
+    return toReturn;
+}
 var checkState = state => decodeURIComponent(state) == `discord_login_test_${process.env.STATE}`;
 const server = http.createServer(async (req, res) => {
     try {
@@ -32,7 +39,7 @@ const server = http.createServer(async (req, res) => {
         });
     } else if (parsed.pathname == '/login') {
         res.writeHead(302, {
-            'Location': `https://discordapp.com/api/oauth2/authorize?response_type=code&client_id=${process.env.CLIENT_ID}&redirect_uri=${encodeURIComponent(process.env.CALLBACK)}&state=${encodeURIComponent(`discord_login_test_${process.env.STATE}`)}&scope=identify%20email`
+            'Location': `https://discordapp.com/api/oauth2/authorize?response_type=code&client_id=${process.env.CLIENT_ID}&redirect_uri=${encodeURIComponent(process.env.CALLBACK)}&state=${encodeURIComponent(`discord_login_test_${process.env.STATE}`)}&scope=identify%20email%20guilds`
         });
         res.end();
     } else if (parsed.pathname == '/callback') {
@@ -58,17 +65,23 @@ const server = http.createServer(async (req, res) => {
                         Authorization: `Bearer ${response.data.access_token}`
                     }
                 }).then(response2 => {
-                    fs.readFile('./login.html', 'utf8', (err, data) => {
-                        if (err) {
-                            res.writeHead(404);
-                            res.end('404 Not Found');
-                        } else {
-                            var toResponse = data.replace('!!!nickname!!!', response2.data.username).replace('!!!tag!!!', response2.data.discriminator).replace('!!!id!!!', response2.data.id).replace('!!!locale!!!', response2.data.locale).replace('!!!email!!!', response2.data.email).replace('!!!mfa!!!', trueOrFalse(response2.data.mfa_enabled, '2단계 인증 사용 중', '2단계 인증을 사용하지 않음')).replace('!!!email_verify!!!', trueOrFalse(response2.data.verified, '인증됨', '인증되지 않음')).replace(/!!!!!!avatar!!!!!!/gi, `https://cdn.discordapp.com/avatars/${response2.data.id}/${response2.data.avatar}.jpg?size=2048`).replace('!!!allTag!!!',`${response2.data.username}#${response2.data.discriminator}`);
-                            res.writeHead(200, {
-                                'content-type': 'text/html; charset=utf-8'
-                            });
-                            res.end(toResponse);
+                    axios.get('https://discordapp.com/api/v6/users/@me/guilds', {
+                        headers: {
+                            Authorization: `Bearer ${response.data.access_token}`
                         }
+                    }).then(response3 => {
+                        fs.readFile('./login.html', 'utf8', (err, data) => {
+                            if (err) {
+                                res.writeHead(404);
+                                res.end('404 Not Found');
+                            } else {
+                                var toResponse = data.replace('!!!nickname!!!', response2.data.username).replace('!!!tag!!!', response2.data.discriminator).replace('!!!id!!!', response2.data.id).replace('!!!locale!!!', response2.data.locale).replace('!!!email!!!', response2.data.email).replace('!!!mfa!!!', trueOrFalse(response2.data.mfa_enabled, '2단계 인증 사용 중', '2단계 인증을 사용하지 않음')).replace('!!!email_verify!!!', trueOrFalse(response2.data.verified, '인증됨', '인증되지 않음')).replace(/!!!!!!avatar!!!!!!/gi, `https://cdn.discordapp.com/avatars/${response2.data.id}/${response2.data.avatar}.jpg?size=2048`).replace('!!!allTag!!!',`${response2.data.username}#${response2.data.discriminator}`).replace(/!!!guilds!!!/gi, guilds(response3.data));
+                                res.writeHead(200, {
+                                    'content-type': 'text/html; charset=utf-8'
+                                });
+                                res.end(toResponse);
+                            }
+                        });
                     });
                 });
             });
